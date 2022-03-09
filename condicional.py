41f5557ba7e5f32d6b430a72bc33b1de381e3aca
matias-volke <volkematias@gmail.com>
2022-03-09 00:39:17 -0300

    condicional.py

---
+++ condicional.py
@@ -0,0 +1,15 @@
+#ingresar numero
+n= input("Number: ")
+#convertir el valor ingresado a un formato numerico
+n=int(n)
+
+#condicionales para definir si el valor ingresado es positivo, negativo o cero
+if n>0:
+    print("n es positivo")
+elif n<0:
+    print("n es negativo")
+else:
+    print("n es cero")
+
+
+    
