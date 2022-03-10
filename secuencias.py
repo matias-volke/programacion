15f4c4587082063e2db707006347ef7d5d0e9dfa
matias-volke <volkematias@gmail.com>
2022-03-10 00:25:03 -0300

    add secuencias.py

---
+++ secuencias.py
@@ -0,0 +1,27 @@
+#Crear un string
+nombre = "Matias"
+#Crear una lista de strings
+nombres = ["Matias", "Pedro","Diego"]
+
+print(nombre[0])
+
+#creamos elemento de tipo tuple
+coordenadas = (10,20)
+
+#creamos elemento tipo set vacio y luego adicionamos elementos
+s = set()
+s.add(1)
+s.add(9)
+#obtener el numero de elementos del set
+len(s)
+
+#crear elementos en formato diccionario
+# Define a dictionary
+mascota = {"Matias": "perro", "Pedro": "gato"}
+print(mascota["Matias"])
+# Adicionar un nuevo valor
+mascota["Juan"] = "hamster"
+print(mascota["Juan"])
+
+
+
